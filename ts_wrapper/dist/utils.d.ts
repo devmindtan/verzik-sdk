5b0eafec845024e1e7cf6d87b5a9321f6d40ef3c
Hazhaz1412 <hoanghuanpham3@gmail.com>
2026-04-04 22:31:01 +0700

    chore: dist remove

--- ts_wrapper/dist/utils.d.ts
+++
@@ -1,5 +0,0 @@
-import type { VerzikError } from "./types";
-export declare function hexToBytes(hex: string): Uint8Array;
-export declare function bytesToHex(bytes: Uint8Array): string;
-export declare function parseError(raw: unknown): VerzikError;
-//# sourceMappingURL=utils.d.ts.map
